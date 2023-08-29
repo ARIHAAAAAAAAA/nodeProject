8dfd353dff17cfc32e725059b3bfee11e5eee0e6
ARIHAAAAAAAAA <ari05050700@gmail.com>
2023-08-29 22:06:21 +0300

    update

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(morgan('dev'))
 const port = 8080;
